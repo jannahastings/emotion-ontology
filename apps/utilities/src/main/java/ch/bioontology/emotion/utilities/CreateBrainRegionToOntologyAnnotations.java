e1f3269783e3410f575213f51f5b5b2eeb8215e0
janna.hastings <janna.hastings@gmail.com>
2013-09-05 12:13:44 +0000

    Updated

--- apps/utilities/src/main/java/ch/bioontology/emotion/utilities/CreateBrainRegionToOntologyAnnotations.java
+++ apps/utilities/src/main/java/ch/bioontology/emotion/utilities/CreateBrainRegionToOntologyAnnotations.java
@@ -4,29 +4,62 @@ import gov.nih.nlm.ncbi.www.soap.eutils.EFetchPubmedServiceStub;
 import gov.nih.nlm.ncbi.www.soap.eutils.EFetchPubmedServiceStub.PubmedArticleType;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.semanticweb.HermiT.Reasoner.ReasonerFactory;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLObject;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.InferenceType;
+import org.semanticweb.owlapi.reasoner.NodeSet;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 
 import owltools.graph.OWLGraphWrapper;
 import owltools.io.ParserWrapper;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class CreateBrainRegionToOntologyAnnotations {
-//	private static final String ID_PREFIX = "MFOEM:";
+	private static final String EM_ID_PREFIX = "MFOEM:";
+	private static final String GO_ID_PREFIX = "GO:";
+	private static final String NBO_ID_PREFIX = "NBO:";
+	private static final String MF_ID_PREFIX = "MF:";
+	
 	private static final int ID_LENGTH = 6;
 	
-	private Map<String,String> namesToIds = new HashMap<String, String>(); 
-	private Map<String,List<String>> papersToIds = new HashMap<String,List<String>>();
-	private List<String> annotationFileRows = new ArrayList<String>();
+	private Map<String,String> uberonNamesToIds = new HashMap<String, String>(); 
+	private Map<String,List<String>> papersToAnnoIds = new HashMap<String,List<String>>();
+	private Map<String,List<String>> annoIdsToOntologyIds = new HashMap<String,List<String>>();
+	private Set<String> annotationFileRows = new HashSet<String>();
+	private Map<String,String> replacementIds = new HashMap<String,String>();
 
+	public void parseReplacementIdFile(String fileName) throws Exception {
+		CSVReader reader = new CSVReader(new FileReader(new File(fileName)));
+
+		String[] header = reader.readNext(); //discard header
+		
+		String[] line = reader.readNext();
+		while (line != null) {
+			String oldId = line[0];
+			String newId = line[1];
+			replacementIds.put(oldId, newId);
+			line = reader.readNext();
+		}
+		
+		reader.close();
+		
+	}
+	
 	public void parseUberonSubsetFile(String ontologyIRI) throws Exception {
 		ParserWrapper pw = new ParserWrapper();
 		OWLGraphWrapper graph = pw.parseToOWLGraph(ontologyIRI);
@@ -37,15 +70,15 @@ public class CreateBrainRegionToOntologyAnnotations {
 				String name = graph.getLabel(obj);
 				String id = graph.getIdentifier(obj);
 				
-				namesToIds.put(name, id);
+				uberonNamesToIds.put(name, id);
 				
 				for (String synonym : graph.getSynonymStrings(obj)) {
-					namesToIds.put(synonym, id);
+					uberonNamesToIds.put(synonym, id);
 				}
 			}
 			
 		}
-		System.out.println("Loaded "+namesToIds.keySet().size()+" anatomical regions");
+		System.out.println("Loaded "+uberonNamesToIds.keySet().size()+" anatomical regions");
 	}
 	
 	public void loadPapersAndOntologyTermsFromFile(String filename) throws Exception {
@@ -58,27 +91,86 @@ public class CreateBrainRegionToOntologyAnnotations {
 		for (String[] line : lines) {
 			
 			String pubMedId = line[0];
-			if (!papersToIds.containsKey(pubMedId)) {
-				papersToIds.put(pubMedId, new ArrayList<String>());
+			if (!papersToAnnoIds.containsKey(pubMedId)) {
+				papersToAnnoIds.put(pubMedId, new ArrayList<String>());
 			}
 			for (int i=1; i<line.length; i++) {
 				String val = line[i];
 				if (!val.isEmpty()) {
-					String ontologyId = zeropad(val,ID_LENGTH);
-					papersToIds.get(pubMedId).add(ontologyId);
+					String ontologyId = EM_ID_PREFIX+zeropad(val,ID_LENGTH);
+					papersToAnnoIds.get(pubMedId).add(ontologyId);
 				}
 			}
-			System.out.println("Got "+papersToIds.get(pubMedId).size()+ " ontology annotations for pubmed "+pubMedId);
+			System.out.println("Got "+papersToAnnoIds.get(pubMedId).size()+ " ontology annotations for pubmed "+pubMedId);
 		}
+	}
+	
+	//Reason over neuroanno.owl, in which the EM IDs that are parsed herein are classified. 
+	//Extract the simple class hierarchy of the neuroanno classes. 
+	//
+	//need that class hierarchy to have the uberon annotations associated to it. 
+	public void getClassificationParents(String emOntologyFile) throws Exception {
+		ParserWrapper pw = new ParserWrapper();
+		OWLGraphWrapper graph = pw.parseToOWLGraph(emOntologyFile);
+		OWLOntology ont = graph.getSourceOntology();
+		OWLReasonerFactory reasonerFactory = new ReasonerFactory();
+		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+		OWLReasoner reasoner = reasonerFactory.createReasoner(ont);
+		graph.setReasoner(reasoner);
+		reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+		
+		for (String uberonId : papersToAnnoIds.keySet()) {
+			for (String annoId : papersToAnnoIds.get(uberonId)) {
+				OWLClass annoClass = graph.getOWLClassByIdentifier(annoId);
+				System.out.println("Got annoClass: "+annoClass);
+				if (annoClass == null) {
+					System.out.println("Got no anno class for id "+annoId);
+					continue;
+				}
+				NodeSet<OWLClass> parents = reasoner.getSuperClasses(annoClass, false);
+				
+				for (OWLClass parent : parents.getFlattened()) {
+					String parentId = graph.getIdentifier(parent);
+					System.out.println("Got parent: "+parent);
+					System.out.println("Parent has ID: "+parentId);
+					System.out.println("Parent has label: "+graph.getLabel(parent));
+					if (shouldIncludeParent(parentId)) {
+						if (!annoIdsToOntologyIds.containsKey(annoId)) {
+							annoIdsToOntologyIds.put(annoId, new ArrayList<String>());
+						}
+						annoIdsToOntologyIds.get(annoId).add(graph.getIdentifier(parent));
+					}
+				}
+				if (annoIdsToOntologyIds.containsKey(annoId)) 
+					System.out.println("Got "+annoIdsToOntologyIds.get(annoId).size()+" parent classes for annotation id "+annoId);
+			}
+		}
+	}
+	
+	private boolean shouldIncludeParent(String identifier) {
+		boolean result = false;
+		
+		if (identifier.startsWith(EM_ID_PREFIX)) {
+			String numericPart = identifier.substring(identifier.indexOf(":")+1);
+			if (Integer.valueOf(numericPart) < 1000) {
+				result = true;
+			}
+		} else if (identifier.startsWith(NBO_ID_PREFIX)
+				|| identifier.startsWith(GO_ID_PREFIX)
+				|| identifier.startsWith(MF_ID_PREFIX)) {
+			result = true;
+		}
+		
+		return result; 
 	}
 	
 	
 	public void lookupCandidateAnnotations() throws Exception {
-		if (namesToIds.size()==0 ||papersToIds.size()==0)
+		if (uberonNamesToIds.size()==0 ||papersToAnnoIds.size()==0)
 			throw new RuntimeException("Empty lists, cannot lookup candidate annotations. Load data from files first. ");
 		
 		//first get abstract text for paper. 
-		for (String pubMedId : papersToIds.keySet()) {
+		for (String pubMedId : papersToAnnoIds.keySet()) {
 			
 			EFetchPubmedServiceStub service = new EFetchPubmedServiceStub();
 			EFetchPubmedServiceStub.EFetchRequest req = new EFetchPubmedServiceStub.EFetchRequest();
@@ -104,14 +196,22 @@ public class CreateBrainRegionToOntologyAnnotations {
 			}
 		
 			//check against all the Uberon labels if the abstract text contains them. 
-			for (String name : namesToIds.keySet()) {
+			for (String name : uberonNamesToIds.keySet()) {
 
 				if (title.contains(name)
 						|| abstractText.contains(name)) {
 					System.out.println("Adding annotation for pmid "+pubMedId+" and region "+name);
 					
-					for (String ontologyId : papersToIds.get(pubMedId)) {
-						annotationFileRows.add(namesToIds.get(name)+" = "+ontologyId);
+					for (String ontologyId : papersToAnnoIds.get(pubMedId)) {
+						if (annoIdsToOntologyIds.containsKey(ontologyId)) {
+							for (String parentId : annoIdsToOntologyIds.get(ontologyId)) {
+								if (replacementIds.containsKey(parentId)) {
+									parentId = replacementIds.get(parentId);
+								}
+								parentId = parentId.substring(parentId.indexOf(":")+1);
+								annotationFileRows.add(uberonNamesToIds.get(name)+" = "+parentId);
+							}
+						}
 					}
 				}
 				
@@ -119,6 +219,7 @@ public class CreateBrainRegionToOntologyAnnotations {
 
 		}
 	}
+	
 	
 	public void writeToOutputFile(String outputFileName) throws Exception {
 		if (annotationFileRows.size()==0 ) {
@@ -128,7 +229,7 @@ public class CreateBrainRegionToOntologyAnnotations {
 		
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
 		
-		writer.write("(uberon-emotion)(curator=janna)(species=ALL)\n");
+		writer.write("(species=ALL) (type=UBEREMO) (curator=JH)\n");
 		
 		for (String line : annotationFileRows) {
 			writer.write(line+"\n");
@@ -148,9 +249,10 @@ public class CreateBrainRegionToOntologyAnnotations {
 	
 	public static final void main(String args[]) throws Exception {
 		CreateBrainRegionToOntologyAnnotations annoCreator = new CreateBrainRegionToOntologyAnnotations();
-		
+		annoCreator.parseReplacementIdFile("output/MFOEM-merge-IDs.csv");
 		annoCreator.parseUberonSubsetFile("output/uberon-subset.owl");
 		annoCreator.loadPapersAndOntologyTermsFromFile("input/paper-annotations.csv");
+		annoCreator.getClassificationParents("C:/Work/Ontologies/mfo-and-emo/emotion-ontology/ontology/internal/neuroanno.owl");
 		annoCreator.lookupCandidateAnnotations();
 		annoCreator.writeToOutputFile("output/uberemo.anno");
 	}
